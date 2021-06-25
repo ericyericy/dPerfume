8efd4a4b4b2c7ad15f17f7c7a1efe4e0227974b6
ericyericy <ericyericy@users.noreply.github.com>
2021-06-25 20:31:34 +0800

    Initial commit

---
+++ all.js
@@ -0,0 +1,34 @@
+const navBar = document.querySelector('.navBar');
+const dropNav = document.querySelector('ul.nav');
+// console.log(navBar, dropNav)
+navBar.addEventListener('click', function(){
+  // console.log(dropNav)
+  dropNav.classList.toggle('active');
+});
+
+
+const navLink = document.querySelectorAll('.navLink');
+const content = document.querySelectorAll('.content');
+
+// console.log(navLink);
+navLink.forEach(function(e){
+  // console.log(e)
+  e.addEventListener('click', function(e){
+    let myPage = e.target.dataset.target;
+    // console.log(myPage);
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(function(item){
+      item.classList.remove('active')
+    })
+    document.querySelector(myPage).classList.toggle('active');
+  });
+})
+
+
+
+const aLink = document.querySelectorAll('a');
+aLink.forEach(function(e){
+  e.addEventListener('click',function(e){
+    e.preventDefault()
+  })      
+});
